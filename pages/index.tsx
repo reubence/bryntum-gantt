c1bf902ffa52dc8f7d402b9f8b79c509aa5f148f
Matthew Clarke <m_d_clarke@hotmail.com>
2023-03-02 13:35:11 +0200

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Bryntum Gantt using Next.js</title>
+        <meta name="description" content="Bryntum Gantt using Next.js" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>Index route</div>
+    </>
+  )
+}
