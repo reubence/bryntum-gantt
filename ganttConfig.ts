634a5bac6a0aa8cde6f7476bf323bd03dd237f34
Reuben Rapose <reuben.rapose@gmail.com>
2023-06-15 01:45:03 +0530

    .npmrc added

--- ganttConfig.ts
+++ ganttConfig.ts
@@ -1,11 +1,4 @@
 import type { BryntumGanttProps } from "@bryntum/gantt-react";
-const ganttConfig: BryntumGanttProps = {
-  columns: [{ type: "name", field: "name", width: 250 }],
-  viewPreset: "weekAndDayLetter",
-  barMargin: 10,
-};
-export { ganttConfig };
-
 import { ProjectModel } from "@bryntum/gantt";
 const project = new ProjectModel({
   taskStore: {
@@ -20,8 +13,18 @@ const project = new ProjectModel({
       url: "http://localhost:3000/api/gantt",
     },
   },
+
   autoLoad: true,
   autoSync: true,
+
   validateResponse: true,
 });
-export { project };
+
+const ganttConfig: BryntumGanttProps = {
+  columns: [{ type: "name", field: "name", width: 250 }],
+  viewPreset: "weekAndDayLetter",
+  barMargin: 10,
+  project,
+};
+
+export { ganttConfig };
